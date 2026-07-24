2aa7e0cec70fda65f12087d11be359bd50cc8188
Saurabh Saw <98752144+saurabh-saw@users.noreply.github.com>
2026-07-25 00:06:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import numpy as np
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
+import tf_keras as keras
 import pickle
 
-# Load the trained model
-model = tf.keras.models.load_model('model.h5')
+# Load model using legacy tf_keras
+model = keras.models.load_model('model.h5')
 
 # Load the encoders and scaler
 with open('label_encoder_gender.pkl', 'rb') as file:
